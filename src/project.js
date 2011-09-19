819a39eed9fbf33cde8bf4ba4e02848e9405daa0
Mikko Ohtamaa <mikko@mfabrik.com>
2011-09-19 03:12:04 +0300

    Added Project schemata.

---
+++ src/project.js
@@ -0,0 +1,60 @@
+var krusovice = krusovice || {};
+
+/**
+ * Project object captures data for server-side rendering job.
+ *
+ * Krusovice uses this model to pass information what kind of
+ * rendering job the server should do. Besides having the
+ * contents of the show, it will also contain metadata for the quality
+ * and ownership. 
+ */
+krusovice.Project = function(cfg) {
+    $.extend(this, cfg);
+}
+
+/**
+ * 
+ */
+krusovice.Project.prototype = {
+
+    /**
+     * @type {Object}
+     *
+     * See {@link krusovice#Design}.
+     */
+    design : null,
+
+	/**
+	 * @type Number
+	 *
+	 * Pixel width of rendering output
+	 */
+	width : 512,
+	
+	
+	/**
+	 * @type Number 
+	 *
+	 * Pixel height of rendering output
+	 */
+	height: 288,
+	
+	
+	/**
+	 * @type String
+	 *
+	 * Email address of the owner of this show. Will be used to send email notification when done.
+	 */
+	email : null,    
+
+
+	/**
+	 * @type String
+	 *
+	 * The server-side rendering process will notify this URL about its progress.
+	 * For internal use only - you cannot set this.	 	
+	 */
+	pingbackURI : null
+    
+
+};
