68521ad35c0d1c8aa2e94f632f2f8e7b3f1d5cae
Mikko Ohtamaa <mikko@mfabrik.com>
2011-09-28 04:10:06 +0300

    Added new effects

--- src/effects/linear.js
+++ src/effects/linear.js
@@ -89,15 +89,21 @@ krusovice.effects.Interpolate = $.extend(true, {}, krusovice.effects.Base, {
         scale = [ scale[0] * baseScale, scale[1] * baseScale, scale[2] * baseScale];
 
         var opacity = source.opacity + (target.opacity-source.opacity)*value;
+        if(mesh.materials.length >= 0) {
+            var material = mesh.materials[0];
+            material.opacity = opacity;
+        }
 
         mesh.position = new THREE.Vector3(position[0], position[1], position[2]);
         mesh.scale = new THREE.Vector3(scale[0], scale[1], scale[2]);
+
 
         // krusovice.utils.calculateAnimation(target.rotation, source.rotation, value);
         var qa = new THREE.Quaternion(source.rotation[0], source.rotation[1], source.rotation[2], source.rotation[3]);
         var qb = new THREE.Quaternion(target.rotation[0], target.rotation[1], target.rotation[2], target.rotation[3]);
 
         THREE.Quaternion.slerp(qa, qb, mesh.quaternion, value);
+
 
         /*
         console.log("Position:" + position);
@@ -163,12 +169,10 @@ krusovice.effects.ZoomIn = $.extend(true, {}, krusovice.effects.Interpolate, {
 
     transitions : ["transitionin", "transitionout"],
 
-
-
-
     init : function() {
         // Override default animation parameters
         this.parameters.source.position = [0, 0, krusovice.effects.BEHIND_CAMERA_Z];
+        this.parameters.source.opacity = 0;
         //this.parameters.opacity.easing = "linear";
     }
 
@@ -246,6 +250,54 @@ krusovice.effects.SlightMove = $.extend(true, {}, krusovice.effects.Interpolate,
 });
 
 krusovice.effects.Manager.register(krusovice.effects.SlightMove);
+
+krusovice.effects.SlightMoveLeftRight = $.extend(true, {}, krusovice.effects.QuaternionRotate, {
+
+    id : "slightmoveleft",
+
+    name : "Slight Move Left -> Right",
+
+    available : true,
+
+    transitions : ["onscreen"],
+
+    easing : "easeInQuad",
+
+    init : function() {
+        // Override default animation parameters
+        var x,y;
+        var r = 0.3;
+        var r2 = 0.1;
+        //this.parameters.source.position = [-krusovice.effects.ON_SCREEN_MAX_X, 0, 0];
+        //this.parameters.target.position = [krusovice.effects.ON_SCREEN_MAX_X, 0, 0];
+
+        x = krusovice.utils.splitrnd(r) * krusovice.effects.ON_SCREEN_MAX_X;
+        y = krusovice.utils.splitrnd(r2) * krusovice.effects.ON_SCREEN_MAX_Y;
+        this.parameters.source.position = [-krusovice.effects.ON_SCREEN_MAX_X*0.3, 0, 0];
+        this.parameters.sourceVariation.position = [x, y, 0];
+
+        x = krusovice.utils.splitrnd(r) * krusovice.effects.ON_SCREEN_MAX_X + r;
+        y = krusovice.utils.splitrnd(r2) * krusovice.effects.ON_SCREEN_MAX_Y;
+        this.parameters.target.position = [+krusovice.effects.ON_SCREEN_MAX_X*0.3, 0, 0];
+        this.parameters.targetVariation.position = [x, y, 0];
+
+        var p = this.parameters;
+
+        p.source.axis = [0,0,1];
+        p.source.angle = Math.PI/10;
+        p.sourceVariation.angle = krusovice.utils.splitrnd(Math.PI/8);
+
+        p.target.axis = [0,0,1];
+        p.targetVariation.axis = [0,0,1];
+        p.target.angle = -Math.PI/10;
+        p.targetVariation.angle = krusovice.utils.splitrnd(Math.PI/8);
+
+    }
+
+});
+
+krusovice.effects.Manager.register(krusovice.effects.SlightMoveLeftRight);
+
 
 /**
  * Randomically rotate object around its Z axis
@@ -329,7 +381,7 @@ krusovice.effects.RotoZoomFar = $.extend(true, {}, krusovice.effects.QuaternionR
 
     transitions : ["transitionin", "transitionout"],
 
-    easing : 'easeInQuad',
+    easing : 'easeInBounce',
 
     init : function() {
         var p = this.parameters;
@@ -338,18 +390,91 @@ krusovice.effects.RotoZoomFar = $.extend(true, {}, krusovice.effects.QuaternionR
         p.source.axis = [0,0,1];
         p.source.angle = Math.PI/2;
         p.sourceVariation.angle = Math.PI*6;
+        p.source.opacity = 0;
 
         p.sourceVariation.position = [krusovice.effects.FAR_Z_MAX_X, krusovice.effects.FAR_Z_MAX_Y, 0];
 
         p.target.axis = [0,0,0];
         p.targetVariation.axis = [0,0,0];
         p.target.angle = 0;
+        p.target.opacity = 1;
     },
-
-
 
 });
 
 krusovice.effects.Manager.register(krusovice.effects.RotoZoomFar);
+
+krusovice.effects.SpinLeft = $.extend(true, {}, krusovice.effects.QuaternionRotate, {
+
+    id : "spinleft",
+
+    name : "Spin Left",
+
+    available : true,
+
+    transitions : ["transitionin", "transitionout"],
+
+    easing : 'easeOutCubic',
+
+    init : function() {
+        var p = this.parameters;
+
+        p.source.position = [-krusovice.effects.ON_SCREEN_MAX_X*2,
+                             0,
+                             -400];
+
+        p.source.axis = [0,1,0];
+        p.source.angle = Math.PI*2/3;
+        p.source.opacity = 0.3;
+        p.sourceVariation.axis = [0.1, 0.1, 0];
+
+        p.sourceVariation.position = [krusovice.effects.ON_SCREEN_MAX_X*0.2, krusovice.effects.ON_SCREEN_MAX_Y*0.2, 0];
+
+        p.target.axis = [0,0,0];
+        p.targetVariation.axis = [0,0,0];
+        p.target.angle = 0;
+        p.target.opacity = 1;
+    },
+
+});
+
+krusovice.effects.Manager.register(krusovice.effects.SpinLeft);
+
+krusovice.effects.SpinRight = $.extend(true, {}, krusovice.effects.QuaternionRotate, {
+
+    id : "spinright",
+
+    name : "Spin Right",
+
+    available : true,
+
+    transitions : ["transitionin", "transitionout"],
+
+    easing : 'easeOutCubic',
+
+    init : function() {
+        var p = this.parameters;
+
+        p.source.position = [krusovice.effects.ON_SCREEN_MAX_X*2,
+                             0,
+                             -400];
+
+        p.source.axis = [0,1,0];
+        p.source.angle = -Math.PI*2/3;
+        p.source.opacity = 0.3;
+        p.sourceVariation.axis = [0.1, 0.1, 0];
+
+        p.sourceVariation.position = [krusovice.effects.ON_SCREEN_MAX_X*0.2, krusovice.effects.ON_SCREEN_MAX_Y*0.2, 0];
+
+        p.target.axis = [0,0,0];
+        p.targetVariation.axis = [0,0,0];
+        p.target.angle = 0;
+        p.target.opacity = 1;
+    },
+
+});
+
+krusovice.effects.Manager.register(krusovice.effects.SpinRight);
+
 
 
