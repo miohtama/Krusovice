1b2d6acc80e388b80a2b6629e9eae548acc500c1
Mikko Ohtamaa <mikko@mfabrik.com>
2011-10-22 03:15:32 +0300

    Effects tests fixed.

--- tests/fast/test-effects.js
+++ tests/fast/test-effects.js
@@ -1,6 +1,8 @@
 'use strict';
 
-var EffectsTest = TestCase("Effects");
+/*global window,finalizeTestCase,assertTrue,assertObject,assertEquals*/
+
+var EffectsTest = function() {};
 
 // Some test data
 
@@ -11,15 +13,17 @@ EffectsTest.prototype.testRandomize = function() {
 
     var src = [1,2,3];
     var r;
-    
+
+    var krusovice = this.krusovice;
+
     r = krusovice.utils.randomize(src, [0,0,0]);
     assertEquals([1,2,3], r);
-    
+
     r = krusovice.utils.randomize(1, 0);
     assertEquals(1, r);
 
     r = krusovice.utils.randomize(1, 99999);
-    assertNotEquals(1, r); 
+    assertNotEquals(1, r);
 };
 
 
@@ -29,25 +33,28 @@ EffectsTest.prototype.testRandomize = function() {
  */
 EffectsTest.prototype.testTransitionInParameters = function() {
 
+
     // Try to create a simple animation parameters
+    var krusovice = this.krusovice;
     var effect = krusovice.effects.Manager.get("zoomin");
     var params = {};
-       
+
+
     assertObject(effect);
-    
+
     effect.prepareParameters("source", params, {}, {});
-    
+
     console.log("Params");
     console.log(params);
-    
+
     // x
     assertEquals(0, params.position[0]);
-    
+
     // y
-    assertEquals(0, params.position[1]);   			
+    assertEquals(0, params.position[1]);
 
     // far z
-    assertTrue(params.position[2] > 10);            
+    assertTrue(params.position[2] > 10);
 
 
 };
@@ -58,19 +65,21 @@ EffectsTest.prototype.testTransitionInParameters = function() {
 EffectsTest.prototype.testTransitionOutParameters = function() {
 
     // Try to create a simple animation parameters
+    var krusovice = this.krusovice;
     var effect = krusovice.effects.Manager.get("zoomfar");
     var params = {};
-    
+    var krusovice = this.krusovice;
+
     effect.prepareParameters("source", params, {}, {});
-    
+
     // x
     assertEquals(0, params.position[0]);
-    
+
     // y
-    assertEquals(0, params.position[1]);            
+    assertEquals(0, params.position[1]);
 
     // far z
-    assertTrue(params.position[2] < 0);            
+    assertTrue(params.position[2] < 0);
 
 };
 
@@ -80,30 +89,32 @@ EffectsTest.prototype.testTransitionOutParameters = function() {
  */
 EffectsTest.prototype.testCalculateAnimation = function() {
 
-	var source = {	
+    var krusovice = this.krusovice;
+
+	var source = {
 			position : [-0.03381065586581826, -0.024750653142109516, 0.9507922390475869],
 			scale : [1,1,1],
 			rotation : [0,0,0,1],
-			opacity : 1,
-	}
-	
-	var target = {	
+			opacity : 1
+	};
+
+	var target = {
 			position : [-0.03381065586581826, -0.024750653142109516, 0.9507922390475869],
 			scale : [1,1,1],
 			rotation : [0,0,0,1],
-			opacity : 1,
-	}
-	
-    var position = krusovice.utils.calculateAnimation(target.position, source.position, 0);    
+			opacity : 1
+	};
+
+    var position = krusovice.utils.calculateAnimation(target.position, source.position, 0);
 	assertTrue(krusovice.utils.isNumber(position[0]));
 
-    position = krusovice.utils.calculateAnimation(target.position, source.position, 0.1);    
+    position = krusovice.utils.calculateAnimation(target.position, source.position, 0.1);
 	assertTrue(krusovice.utils.isNumber(position[0]));
-	
-    position = krusovice.utils.calculateAnimation(target.position, source.position, 1);    
+
+    position = krusovice.utils.calculateAnimation(target.position, source.position, 1);
 	assertTrue(krusovice.utils.isNumber(position[0]));
-   
-}
 
+};
 
-          
+finalizeTestCase("Effects", EffectsTest);
+
