6d133e90089bbf4b3f5a444cb685d9b6ed8e98cf
Mikko Ohtamaa <mikko@mfabrik.com>
2011-10-22 03:32:39 +0300

    Text tests pass

--- tests/fast/test-text.js
+++ tests/fast/test-text.js
@@ -1,13 +1,15 @@
+/*global window,finalizeTestCase,assertTrue,assertObject,assertEquals,assertNotEquals*/
+
 'use strict';
 
-/*global TestCase*/
-
-var TextTest = window.TestCase("Text");
+var TextTest = function() {};
 
 /**
  * Render transition in frame for an test image element
  */
 TextTest.prototype.testRenderBasicText = function() {
+
+    var krusovice = this.krusovice;
 
 	var init = this.basicSetup();
 
@@ -15,41 +17,40 @@ TextTest.prototype.testRenderBasicText = function() {
 		data : init.elem,
 		renderer : init.renderer
 	});
-	
+
 	object.prepare(null, 512, 512);
 
     object.animate(0);
-    
-};
 
+};
 
 
 /**
  * Create a single timeline element for testing purposes.
  */
 TextTest.prototype.createTimelineElement = function() {
-    
+    var krusovice = this.krusovice;
     var elements = [
         {
             "id" : 0,
             "type" : "text",
             "labels" : {
-                "text" : "Testing"   
+                "text" : "Testing"
             },
             "shape" : "box"
         }
     ]
-    
+
 	var timeliner = krusovice.Timeliner.createSimpleTimeliner(elements, null);
 	var plan = timeliner.createPlan();
 
 	return plan[0];
-}
+};
 
 
 TextTest.prototype.basicSetup = function() {
-    
- 
+
+    var krusovice = this.krusovice;
 	var renderer = new krusovice.renderers.Three({
 		width: 100,
 		height : 100
@@ -64,6 +65,8 @@ TextTest.prototype.basicSetup = function() {
 		renderer : renderer
 	};
 
-}
+};
+
+finalizeTestCase("Text", TextTest);
 
 
