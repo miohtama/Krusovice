b5e04f98791dd6c219c2d27ed0ee71862c153e0d
Mikko <mikko@mfabrik.com>
2011-11-20 18:24:32 +0200

    Added example for transparent text rotate

--- tests/manual/framed-mesh.js
+++ tests/manual/framed-mesh.js
@@ -8,15 +8,16 @@
 
 require(["krusovice/thirdparty/jquery",
     "krusovice/api",
-    "../../src/thirdparty/domready!"], function($, krusovice) {
+    "krusovice/tools/text2canvas",
+    "../../src/thirdparty/domready!"], function($, krusovice, text2canvas) {
 
     "use strict";
 
     var renderer = new krusovice.renderers.Three({ width : 640, height: 360, webGL : true});
-    var mesh = null;
+
     var i = 0;
 
-    function loop() {
+    function loop(mesh, mesh2) {
         var canvas = document.getElementsByTagName("canvas")[0];
         if(!canvas) { throw "Ooops"; }
         var ctx = canvas.getContext("2d");
@@ -30,8 +31,20 @@ require(["krusovice/thirdparty/jquery",
         //mesh.updateMatrix();
         mesh.updateMatrixWorld();
 
+
+        mesh2.rotation.y -= 0.01;
+        //mesh.rotation.x += 0.02;
+
+        //mesh.updateMatrix();
+        mesh2.updateMatrixWorld();
+
         renderer.render(ctx);
-        krusovice.utils.requestAnimationFrame(loop);
+
+        function again() {
+            loop(mesh, mesh2);
+        }
+
+        krusovice.utils.requestAnimationFrame(again);
         //console.log("Frame:" + i++);
     }
 
@@ -49,7 +62,7 @@ require(["krusovice/thirdparty/jquery",
         plane.materials[0] = bodyMaterial;
         plane.materials[1] = bodyMaterial;
         //mesh = new THREE.Mesh(plane, material);
-        mesh = new THREE.Mesh(plane, material);
+        var mesh = new THREE.Mesh(plane, material);
         //mesh.doubleSided = true;
         //mesh.useQuaternion = true;
         //mesh.position = [0,0,krusovice.effects.ON_SCREEN_Z];
@@ -57,6 +70,8 @@ require(["krusovice/thirdparty/jquery",
         //mesh.rotation.y = 2;
 
         renderer.scene.addObject(mesh);
+
+        return mesh;
     }
 
     function init() {
@@ -64,10 +79,37 @@ require(["krusovice/thirdparty/jquery",
 
         var img = new Image();
 
-        function done() {
+
+        function createImage() {
             var texture = new THREE.Texture(img, THREE.UVMapping);
-            createObjects(texture, img);
-            loop();
+            var mesh = createObjects(texture, img);
+
+            mesh.position.y += 0.5;
+
+            return mesh;
+        }
+
+        function createText() {
+            var canvas = document.createElement("canvas");
+            canvas.width = 512;
+            canvas.height = 288;
+
+            var renderer = new text2canvas.Renderer({canvas:canvas});
+
+            renderer.renderText("Foobar");
+
+            var texture = new THREE.Texture(canvas, THREE.UVMapping);
+            var mesh = createObjects(texture, canvas);
+
+            mesh.position.y -= 0.5;
+
+            return mesh;
+        }
+
+        function done() {
+            var mesh = createImage();
+            var mesh2 = createText();
+            loop(mesh, mesh2);
         }
 
         //var material = new THREE.MeshFaceMaterial();
